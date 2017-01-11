598cef0d640d82b1bf6df336942710806a2e3883
cristiangabor <cristi26.gabor@gmail.com>
2017-01-12 00:43:15 +0200

    Add xml

--- Server/servemonitor.py
+++ Server/servemonitor.py
@@ -2,38 +2,20 @@ import socket
 import sys
 from _thread import *
 from simplecrypt import decrypt
+import xml.etree.ElementTree as ET
 
-# 1.Gets the local ip/ip over LAN.
 
-HOST =socket.gethostbyname(socket.gethostname())
-
-# 2.Use port no. above 1800 so it does not interfere with ports already in use.
-
-PORT =input ("Enter the PORT number (1 - 10,000)")
-
-CLIENT_IP=None
-CLIENT_PORT=None
-
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.bind((HOST, int(PORT)))
-except socket.error as msg:
-    print(str(msg))
-
-s.listen(100)
-print("Waiting for a connection....")
 
 def decrypt_data(data, CLIENT_IP, CLIENT_PORT):
     f = open("mydata.txt",'a+')
     if data:
         print("Decrypting the information")
-        decrypted_data = decrypt('cristian', data).decode('utf-8')
+        decrypted_data = decrypt('cris', data).decode('utf-8')
         print(decrypted_data)
         CLIENT_IP,CLIENT_PORT=addr[0],addr[1]
         aditional_info="\n" +"CLIENT_IP: " + str(CLIENT_IP) + " " + "CLIENT_PORT: " + str(CLIENT_PORT) + " "
         aditional_info +=str(decrypted_data)
-        f.write(str(adtional_info))
+        f.write(str(aditional_info))
         f.close()
         return(decrypted_data)
     else:
@@ -54,8 +36,39 @@ def threded_clinet(conn,addr):
         conn.sendall(str.endcode(reply))
     conn.close()
 
-while True:
-    conn ,addr = s.accept()
-    print("Connected to :"+  addr[0] + ":" + str(addr[1]))
 
-    start_new_thread(threded_clinet, (conn,addr,))
+def parsing(filepath):
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+    print(root)
+
+
+def main():
+    # 1.Gets the local ip/ip over LAN.
+
+    HOST =socket.gethostbyname(socket.gethostname())
+
+    # 2.Use port no. above 1800 so it does not interfere with ports already in use.
+
+    PORT =input ("Enter the PORT number (1 - 10,000)")
+
+    CLIENT_IP=None
+    CLIENT_PORT=None
+
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        s.bind((HOST, int(PORT)))
+    except socket.error as msg:
+        print(str(msg))
+
+    s.listen(100)
+    print("Waiting for a connection....")
+
+    while True:
+        conn ,addr = s.accept()
+        print("Connected to :"+  addr[0] + ":" + str(addr[1]))
+
+        start_new_thread(threded_clinet, (conn,addr,))
+
+main()
