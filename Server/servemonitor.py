fbe54aba6f8765ddc229181ed91eee2f9b98050c
cristiangabor <cristi26.gabor@gmail.com>
2017-01-11 13:52:49 +0200

    Server added

--- Server/servemonitor.py
+++ Server/servemonitor.py
@@ -4,26 +4,38 @@ from _thread import *
 from simplecrypt import decrypt
 
 # 1.Gets the local ip/ip over LAN.
+
 HOST =socket.gethostbyname(socket.gethostname())
+
 # 2.Use port no. above 1800 so it does not interfere with ports already in use.
+
 PORT =input ("Enter the PORT number (1 - 10,000)")
-BUFFER_SIZE = 1024
+
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    s.bind((HOST, PORT))
+    s.bind((HOST, int(PORT)))
 except socket.error as msg:
     print(str(msg))
 
 s.listen(100)
 print("Waiting for a connection....")
 
+def decrypt_data(data):
+    if data:
+	print("Decrypting the information")
+	decrypted_data = decrypt('cristian', data).decode('utf-8')
+	return(decrypted_data)
+    else:
+	print("There is no data to decrypt")
+   	
+
+
 def threded_clinet(conn):
-    conn.send(str.encode("Welcome user!\n"))
-    data = conn.recv(20)
-    decrpted_data = decrypt('cristian', data)
-    print(decrpted_data)
+    conn.send(str.encode("Data received by the server!\n"))
+    data = conn.recv(1024)
+    decrypt_data(data)
     if data:
         reply = "Server output: Data was received by the server!"
         conn.sendall(str.encode(reply))
@@ -34,6 +46,6 @@ def threded_clinet(conn):
 
 while True:
     conn ,addr = s.accept()
-    print("Connected to :" + addr[0] + ":" + str(addr[1]))
+    print("Connected to :"+  addr[0] + ":" + str(addr[1]))
 
-    #start_new_thread(threded_clinet, (conn,))
+    start_new_thread(threded_clinet, (conn,))
